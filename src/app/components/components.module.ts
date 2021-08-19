2e4ce55a71c54b2abed20ba594317e92fb0acc39
Diego <diegoandrestrom04@gmail.com>
2021-08-19 10:04:00 -0500

    first commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { IonicModule } from '@ionic/angular';
+
+
+@NgModule({
+  declarations: [
+    CardComponent
+  ],
+  exports: [
+    CardComponent
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ]
+})
+export class ComponentsModule { }
